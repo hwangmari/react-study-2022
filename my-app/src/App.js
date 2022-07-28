09c8d09f27e92e170e17b892dd668a2d5f922bde
linda.hwang <hwangmari@naver.com>
2022-07-29 02:13:19 +0900

    16 state props 와 render 함수
     함수 뒤에 .bind(this) 추가하기
    this.setState 함수를 호출해서 state 값 변경하기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,10 +8,12 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: "read",
       subject: {
         title: "WEB",
         sub: "하이?",
       },
+      welcome: { title: "Welcome", desc: " React!!!!" },
       contents: [
         { id: 1, title: "HTML 되고있냐?", desc: "HTML..." },
         { id: 2, title: "CSS", desc: "CSS..." },
@@ -20,14 +22,38 @@ class App extends Component {
     };
   }
   render() {
+    console.log("App render");
+    var _title,
+      _desc = null;
+    if (this.state.mode === "welcome") {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if (this.state.mode === "read") {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
-        <Subject
+        {/* <Subject
           title={this.state.subject.title}
           sub={this.state.subject.sub}
-        ></Subject>
+        ></Subject> */}
+        <header>
+          <h1>
+            <a
+              href="/"
+              onClick={function (e) {
+                e.preventDefault(); // HTML태그가 가진 고유의 이벤트를 동작하지 못하게 해야할때 사용.
+                this.setState({ mode: "welcome" });
+              }.bind(this)}
+            >
+              {this.state.subject.title}
+            </a>
+          </h1>
+          {this.state.subject.sub}
+        </header>
         <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is ..."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
