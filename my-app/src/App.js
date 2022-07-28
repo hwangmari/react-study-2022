7ca2b328275136b2616a34b93078969d883f5258
linda.hwang <hwangmari@naver.com>
2022-07-29 01:51:28 +0900

    15. state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: {
+        title: "WEB",
+        sub: "하이?",
+      },
+      contents: [
+        { id: 1, title: "HTML 되고있냐?", desc: "HTML..." },
+        { id: 2, title: "CSS", desc: "CSS..." },
+        { id: 3, title: "JaveScript", desc: "JaveScript..." },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        ></Subject>
+        <TOC data={this.state.contents}></TOC>
+        <Content title="HTML" desc="HTML is ..."></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
